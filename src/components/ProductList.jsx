e03ea6c00e10fff87bf453733638c51d6b9b55a4
BRC-Thomas <thomasbaracani@gmail.com>
2024-01-16 15:52:00 +0100

    cart logic

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
 import { getProductsList } from "../features/product";
+import { addOneToCart } from "../features/cart";
 
 export default function ProductList() {
   const products = useSelector((state) => state.products);
@@ -9,20 +10,24 @@ export default function ProductList() {
     dispatch(getProductsList());
   }
 
+  console.log(products.items);
+
   return (
     <div className="px-6">
       <h1 className="text-slate-100 text-2xl mb-6">Here are our products</h1>
       <ul className="grid min-[500px]:grid-cols-2 md:grid-cols-3 gap-4">
         {products.items &&
-          products.items.map((element) => (
-            <li key={element.id} className="p-4 bg-slate-200 rounded">
-              <img src={`/images/${element.img}.png`} alt={element.title} />
+          products.items.map((el) => (
+            <li key={el.id} className="p-4 bg-slate-200 rounded">
+              <img src={`/images/${el.img}.png`} alt={el.title} />
               <div className="flex justify-between items-center mb-6">
-                <p className="text-slate-700 text-lg">{element.title}</p>
-                <p className="text-slate-900 text-bold">{element.price}</p>
+                <p className="text-slate-700 text-lg">{el.title}</p>
+                <p className="text-slate-900 text-bold">{el.price}</p>
               </div>
-              <button className={`${element.picked ? "bg-green-700" : "bg-slate-600"} w-full text-slate-100 px-2 inline-flex items-center justify-center rounded p-2 `}>
-                {element.picked ? "Item picked ✅" : "Add to cart"}
+              <button 
+              onClick={() => dispatch(addOneToCart(el.id))}
+              className={`${el.picked ? "bg-green-700" : "bg-slate-600"} w-full text-slate-100 px-2 inline-flex items-center justify-center rounded p-2 `}>
+                {el.picked ? "Item picked ✅" : "Add to cart"}
               </button>
             </li>
           ))}
